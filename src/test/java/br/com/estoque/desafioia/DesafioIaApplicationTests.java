4566a8643207980c70dccce868c40172a19472bd
thiagotnunes08 <thiagotomaznunes.9908@outlook.com>
2023-01-19 18:29:10 -0300

    initial commit

---
+++ src/test/java/br/com/estoque/desafioia/DesafioIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoque.desafioia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
